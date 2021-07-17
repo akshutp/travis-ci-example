d12a27fa7d76aa9a16419eed605b206294fa9728
akshutp <akshayatp16@gmail.com>
2021-07-17 22:46:46 +0530

    first commit

---
+++ src/test/java/com/akshaya/ci/travisciexample/TravisCiExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akshaya.ci.travisciexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravisCiExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
